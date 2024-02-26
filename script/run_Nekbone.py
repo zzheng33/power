5a2153d58a15d7f3a00726dac4667249644c5c1b
zzheng33 <zz695920754@gmail.com>
2024-02-26 16:31:50 -0600

    add files

--- script/run_Nekbone.py
+++ script/run_Nekbone.py
@@ -1,1 +1,4 @@
 # https://asc.llnl.gov/sites/asc/files/2021-02/Nekbone_Summary_2_3_6.pdf
+# https://github.com/Nek5000/Nekbone/blob/master/USERGUIDE.pdf
+
+# modify the makenek file to change the src dir
