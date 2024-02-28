767366101083c0de173735a0f9a21bd0c8097a33
zzheng33 <zz695920754@gmail.com>
2024-02-28 16:13:28 -0600

    add miniFE

---
+++ benchmark/miniFE/ref/src/miniFE_info.hpp
@@ -0,0 +1,13 @@
+#ifndef miniFE_info_hpp
+#define miniFE_info_hpp
+
+#define MINIFE_HOSTNAME "bush"
+#define MINIFE_KERNEL_NAME "'Linux'"
+#define MINIFE_KERNEL_RELEASE "'5.15.0-46-generic'"
+#define MINIFE_PROCESSOR "'x86_64'"
+
+#define MINIFE_CXX "'/usr/bin/mpicxx'"
+#define MINIFE_CXX_VERSION "'g++ (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0'"
+#define MINIFE_CXXFLAGS "'-O3 -g'"
+
+#endif
