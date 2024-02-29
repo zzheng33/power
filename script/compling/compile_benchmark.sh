cbe3d508fa8fb691189b03929ee8c12e6b747b2a
zzheng33 <zz695920754@gmail.com>
2024-02-28 22:27:24 -0600

    add compiling script

--- script/compling/compile_benchmark.sh
+++ script/compling/compile_benchmark.sh
@@ -3,7 +3,8 @@
 # 2. write script that clone the github repo to a current sever and compile
 # sudo apt-get update
 # for NPB
-# sudo apt-get install gfortran
-# sudo apt-get install libopenmpi-dev
-# sudo apt-get install mpich
+sudo apt-get update
+sudo apt-get --assume-yes install gfortran
+sudo apt-get --assume-yes install libopenmpi-dev
+sudo apt-get --assume-yes install mpich
 
