8cd24f986a84e104ccb7c84acecb60856f4f673e
zzheng33 <zz695920754@gmail.com>
2024-02-28 19:19:35 -0600

    .

--- script/compling/compile_benchmark.py
+++ script/compling/compile_benchmark.py
@@ -1,3 +1,8 @@
 # to do 
 # 1. upload benchmarks to github repo
 # 2. write script that clone the github repo to a current sever and compile
+# sudo apt-get update
+# for NPB
+# sudo apt-get install gfortran
+# sudo apt-get install libopenmpi-dev
+# sudo apt-get install mpich
