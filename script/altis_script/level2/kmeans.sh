47eb5676f588f7376003fbe4bd84e79eb32ffae9
zzheng1999 <zzheng27@hawk.iit.edu>
2024-03-05 15:16:19 -0600

    .

--- script/altis_script/level2/kmeans.sh
+++ script/altis_script/level2/kmeans.sh
@@ -4,4 +4,4 @@ home_dir=$HOME
 benchmark_dir="${home_dir}/benchmark/altis/build/bin/level2"
 data_dir="${home_dir}/benchmark/altis/data/kmeans/kmeans_f_8388608_30"
 
-"$benchmark_dir/kmeans" -i "$data_dir"
+"$benchmark_dir/kmeans" -i "$data_dir" --passes 5
