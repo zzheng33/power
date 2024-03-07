22abc5bf4f946ad6d86810a7c73f97d3bed9e4eb
zzheng33 <zz695920754@gmail.com>
2024-03-07 15:44:56 -0600

    /

--- script/altis_script/level0/.ipynb_checkpoints/busspeedreadback-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-home_dir=$HOME
-benchmark_dir="${home_dir}/benchmark/altis/build/bin/level0"
-
-
-"$benchmark_dir/busspeedreadback" 
--- script/ecp_script/.ipynb_checkpoints/run_miniFE-checkpoint.py
+++
@@ -1,2 +0,0 @@
-# https://asc.llnl.gov/sites/asc/files/2020-06/MiniFE_Summary_v2.0.pdf
-# ./miniFE.x -nx 265 -ny 256 -nz 256 
