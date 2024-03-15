d3625e8386df154553494146ff782ad7b2e04556
Chameleon Cloud User <cc@power.novalocal>
2024-03-15 19:15:35 +0000

    /

--- script/start_power_motif.sh
+++ script/start_power_motif.sh
@@ -2,10 +2,13 @@
 
 
 # test
-./power_util/cpu_cap.sh 110
-./power_util/gpu_cap.sh 110
+./power_util/cpu_cap.sh 170
+./power_util/gpu_cap.sh 130
 
 #python3 exp_power_motif.py  --test 1 --suite 1
 
-# run all benchamrks
-python3 exp_power_motif.py --suite 0 --benchmark CRADL --test 1
+
+
+# python3 exp_power_motif.py --suite 0 --benchmark CRADL --test 1
+
+python3 exp_power_motif.py --suite 0 --benchmark sw4lite --test 1
