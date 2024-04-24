8f95886a3285ec13ee6148fc9810b622f2588464
zzheng33 <zz695920754@gmail.com>
2024-04-24 14:35:04 +0000

    /

--- script/run_benchmark/npb_script/miniFE.sh
+++ script/run_benchmark/npb_script/miniFE.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 home_dir=$HOME
+# benchmark_dir="${home_dir}/benchmark/CPU-only/miniFE/openmp/src/"
 benchmark_dir="${home_dir}/benchmark/CPU-only/miniFE/ref/src/"
 
 cd ${benchmark_dir}
-./miniFE.x -nx 200 -ny 200 -nz 200 
+mpirun -n 16 --use-hwthread-cpus ./miniFE.x  -nx 300 -ny 300 -nz 300
+# mpirun -n 48 --use-hwthread-cpus ./miniFE.x  -nx 528 -ny 512 -nz 512
+# ./miniFE.x  -nx 300 -ny 300 -nz 300
