835b73075b1d81c934056f1f58414bf02c124993
zzheng33 <zzheng33@uic.edu>
2024-05-01 13:54:42 +0000

    /

--- data/filter_performance_csv.py
+++ data/filter_performance_csv.py
@@ -4,14 +4,14 @@ import numpy as np
 import os  
 from matplotlib.ticker import PercentFormatter
 from scipy.optimize import curve_fit
-base_path = '/home/cc/power/data/npb_performance'
+base_path = '/home/cc/power/data/npb_performance/'
 # List of second header values you are interested in
 desired_columns = ['Time','EXEC','INST','PhysIPC%', 'CFREQ', 'READ', 'WRITE', 'Proc Energy (Joules)', 'DRAM Energy (Joules)', 'UncFREQ (Ghz)']
 
 # Function to filter and overwrite a CSV file
 def filter_csv(file_path):
     # Read the CSV file with a two-row header
-    df = pd.read_csv(file_path, header=[0, 1])
+    df = pd.read_csv(file_path, header=[0, 1], on_bad_lines='skip')
     # Filter columns based on specified headers
     filtered_df = df.loc[:, (df.columns.get_level_values(0) == 'System') & (df.columns.get_level_values(1).isin(desired_columns))]
     # Save the filtered DataFrame back to CSV, overwriting the original file
