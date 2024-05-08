c41a86aa1cdcb6103f9a214abb22f0800ef36926
zzheng33 <zzheng33@uic.edu>
2024-05-08 13:40:31 +0000

    /

--- script/run_benchmark/ecp_script/LULESH.sh
+++ script/run_benchmark/ecp_script/LULESH.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 home_dir=$HOME
-benchmark_dir="${home_dir}/benchmark/CPU-only/LULESH"
-benchmark_dir="/home/cc/benchmark/CPU-only/LULESH"
+benchmark_dir="${home_dir}/benchmark/ECP/LULESH"
+benchmark_dir="/home/cc/benchmark/ECP/LULESH"
 
 "$benchmark_dir/lulesh2.0" -s 40
