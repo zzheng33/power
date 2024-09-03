ad69b1d236fdfec942a6f7eea47c4f13b378c0f7
zzheng33 <zz695920754@gmail.com>
2024-09-03 16:35:46 +0000

    /

--- ML/script/.ipynb_checkpoints/start_power_motif-checkpoint.py
+++ ML/script/.ipynb_checkpoints/start_power_motif-checkpoint.py
@@ -28,24 +28,27 @@ subprocess.run(pm_command, shell=True)
 
 def run_benchmark(model, test):
     if not test:
-        output_cpu = f"./data/{benchmark}_power_cpu.csv"
-        output_gpu = f"./data/{benchmark}power_gpu.csv"
+        output_cpu = f"../data/{model}/{model}_power_cpu.csv"
+        output_gpu = f"../data/{model}/{model}_power_gpu.csv"
     else:
-        output_cpu = f"./data/test/{benchmark}_power_cpu.csv"
-        output_gpu = f"./data/test/{benchmark}_power_gpu.csv"
+        output_cpu = f"../data/test/{model}_power_cpu.csv"
+        output_gpu = f"../data/test/{model}_power_gpu.csv"
     
     # run_benchmark_command = f"{python_executable} {train} --model {model}"
-    run_benchmark_command = f"{python_executable} {train}"
+    train_model_command = f"{python_executable} {train}"
         
 
-    benchmark_process = subprocess.Popen(run_benchmark_command, shell=True)
+    benchmark_process = subprocess.Popen(train_model_command, shell=True)
     benchmark_pid = benchmark_process.pid
 
-
+    monitor_command_cpu = f"echo 9900 | sudo -S {python_executable} {read_cpu_power}  --output_csv {output_cpu} --pid {benchmark_pid}"
+    monitor_process = subprocess.Popen(monitor_command_cpu, shell=True, stdin=subprocess.PIPE, text=True)
+    
     # Start GPU power monitoring, passing the PID of the benchmark process
     monitor_command_gpu = f"echo 9900 | sudo -S {python_executable} {read_gpu_power}  --output_csv {output_gpu} --pid {benchmark_pid}"
     monitor_process = subprocess.Popen(monitor_command_gpu, shell=True, stdin=subprocess.PIPE, text=True)
 
+    
     # Wait for the benchmark process to complete
     benchmark_exit_code = benchmark_process.wait()
 
@@ -55,8 +58,8 @@ def run_benchmark(model, test):
 if __name__ == "__main__":
    # Set up command line argument parsing
     parser = argparse.ArgumentParser(description='Run ML models and monitor power consumption.')
-    parser.add_argument('--model', type=str, help='Optional name of the model to train', default=None)
-    parser.add_argument('--test', type=int, help='whether it is a test run', default=None)
+    parser.add_argument('--model', type=str, help='Optional name of the model to train', default="resnet50")
+    parser.add_argument('--test', type=int, help='whether it is a test run', default=0)
 
     args = parser.parse_args()
     model = args.model
