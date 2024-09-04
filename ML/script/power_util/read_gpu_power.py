5ca2eabb40a7af1a152cd61d8d79411db1660567
zzheng33 <zz695920754@gmail.com>
2024-09-04 00:17:26 +0000

    /

--- ML/script/power_util/read_gpu_power.py
+++ ML/script/power_util/read_gpu_power.py
@@ -6,47 +6,13 @@ import psutil
 import subprocess  # Import subprocess to run nvidia-smi
 import pandas as pd
 
-# Function to get current GPU power usage
-# def get_gpu_power():
-#     try:
-#         # Run the nvidia-smi command to get power usage, parse it
-#         smi_output = subprocess.check_output(['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'], encoding='utf-8')
-#         power_draw = float(smi_output.strip())  # Convert power draw from string to float
-#         return power_draw
-#     except Exception as e:
-#         print(f"Error getting GPU power usage: {e}")
-#         return 0
-
-# # Function to monitor power consumption of GPU
-# def monitor_gpu_power(benchmark_pid, output_csv, interval=0.3):
-#     start_time = time.time()
-#     power_data = []
-
-#     while psutil.pid_exists(benchmark_pid):
-#         time.sleep(interval)
-#         current_time = time.time()
-#         elapsed_time = current_time - start_time
-
-#         gpu_power = get_gpu_power()
-
-#         power_data.append([elapsed_time, gpu_power])
-
-#     os.makedirs(os.path.dirname(output_csv), exist_ok=True)
-#     with open(output_csv, 'w', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['Time (s)', 'GPU Power (W)'])
-#         writer.writerows(power_data)
-
 def get_gpu_power():
-    try:
-        # Run the nvidia-smi command to get power usage for all GPUs, parse it
-        smi_output = subprocess.check_output(['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'], encoding='utf-8')
-        # Split the output by lines, convert each one from string to float
-        power_draws = [float(power_draw.strip()) for power_draw in smi_output.strip().split('\n')]
-        return power_draws
-    except Exception as e:
-        print(f"Error getting GPU power usage: {e}")
-        return []
+    
+    # Run the nvidia-smi command to get power usage for all GPUs, parse it
+    smi_output = subprocess.check_output(['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'], encoding='utf-8')
+    # Split the output by lines, convert each one from string to float
+    power_draws = [float(power_draw.strip()) for power_draw in smi_output.strip().split('\n')]
+    return power_draws
 
 # Function to monitor power consumption of all GPUs
 def monitor_gpu_power(benchmark_pid, output_csv, avg, interval=0.3):
