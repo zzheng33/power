a412cb24b466db3c6e82e480c433bb241df684a1
zzheng33 <zz695920754@gmail.com>
2024-09-04 16:25:15 +0000

    /

--- GPGPU/script/run_benchmark/altis_script/level2/.ipynb_checkpoints/fdtd2d-checkpoint.sh
+++ GPGPU/script/run_benchmark/altis_script/level2/.ipynb_checkpoints/fdtd2d-checkpoint.sh
@@ -5,4 +5,4 @@ benchmark_dir="${home_dir}/benchmark/altis/build/bin/level2"
 
 benchmark_dir="/home/cc/benchmark/altis/build/bin/level2"
 
-taskset -c 0 "$benchmark_dir/fdtd2d" --passes 1 -s 4
+"$benchmark_dir/fdtd2d" --passes 3 -s 4
