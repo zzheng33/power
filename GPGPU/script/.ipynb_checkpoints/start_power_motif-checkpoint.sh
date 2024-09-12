19be78ce364e8547a1cb238625e9cc04c9a1cfc8
zzheng33 <zz695920754@gmail.com>
2024-09-12 18:51:59 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_power_motif-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_power_motif-checkpoint.sh
@@ -9,7 +9,7 @@
 # ./power_util/cpu_cap.sh 250
 # ./power_util/gpu_cap.sh 260
 
-python3 exp_power_motif.py --suite 1 --test 0 --benchmark lavamd --dynamic_uncore_fs 1
+python3 exp_power_motif.py --suite 1 --test 0 --benchmark kmeans --dynamic_uncore_fs 0 --low_uncore 0
 
 
 # python3 exp_power_motif.py --suite 0 --benchmark CRADL --test 1
