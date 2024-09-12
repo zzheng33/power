6930cfd853eb6779f16db918d7af901d5219f094
zzheng33 <zz695920754@gmail.com>
2024-09-12 21:48:45 +0000

    /

--- GPGPU/script/start_power_motif.sh
+++ GPGPU/script/start_power_motif.sh
@@ -9,7 +9,7 @@
 # ./power_util/cpu_cap.sh 250
 # ./power_util/gpu_cap.sh 260
 
-python3 exp_power_motif.py --suite 1 --test 0  --dynamic_uncore_fs 0 --low_uncore 0 --benchmark busspeeddownload
+python3 exp_power_motif.py --suite 1 --test 0  --dynamic_uncore_fs 1 --low_uncore 0 --benchmark gemm
 
 
 # python3 exp_power_motif.py --suite 0 --benchmark CRADL --test 1
