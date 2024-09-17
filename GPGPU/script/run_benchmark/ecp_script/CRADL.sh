f1b29fadf43e43785bdda26e448804819e13d7a3
zzheng33 <zz695920754@gmail.com>
2024-09-17 16:46:33 +0000

    /

--- GPGPU/script/run_benchmark/ecp_script/CRADL.sh
+++ GPGPU/script/run_benchmark/ecp_script/CRADL.sh
@@ -6,7 +6,6 @@ benchmark_dir="${home_dir}/benchmark/ECP/CRADL/"
 
 cd ${benchmark_dir}
 source CRADL_env/bin/activate
-
-mpirun -n 12 python cradl_benchmarking.py --inference_dir ./data
+python cradl_benchmarking.py --inference_dir ./data
 
 deactivate
