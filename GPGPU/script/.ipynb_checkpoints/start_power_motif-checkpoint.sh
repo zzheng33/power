cedcca3654dc262b8da85d0a094e869032147f06
zzheng33 <zz695920754@gmail.com>
2024-09-20 01:16:44 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_power_motif-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_power_motif-checkpoint.sh
@@ -16,7 +16,7 @@
 # CARDL: 3000,3000, 3
 # UNet:  200, 500, 5
 # miniGAN 200, 500, 5
-python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --benchmark miniGAN
+python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.4 --uncore_1 2.4 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --benchmark Laghos
 
 ./power_util/set_uncore_freq.sh 2.4 2.4
 
