02e2da36fd73617bfc0d5a6b58a65ebc81e2bb73
zzheng33 <zz695920754@gmail.com>
2024-09-20 01:18:37 +0000

    /

--- GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/Laghos-checkpoint.sh
+++ GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/Laghos-checkpoint.sh
@@ -5,4 +5,4 @@ benchmark_dir="${home_dir}/benchmark/ECP/Laghos/"
 
 cd ${benchmark_dir}
 
-./laghos -p 3 -m data/cube_922_hex.mesh -rs 1 -tf 5.0 -pa -cgt 1e-12 -d cuda
+./laghos -p 3 -m data/box01_hex.mesh -rs 1 -tf 5.0 -pa -cgt 1e-12 -d cuda
