0b0d9ee33ed5789fe15346837bb95fdaa890dc2e
zzheng33 <zz695920754@gmail.com>
2024-09-20 02:40:55 +0000

    /

--- GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/Resnet50-checkpoint.sh
+++ GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/Resnet50-checkpoint.sh
@@ -5,8 +5,7 @@ benchmark_dir="${home_dir}/benchmark/ECP/Resnet50/"
 
 source ${home_dir}/benchmark/ECP/UNet/UNet_env/bin/activate
 
-cd benchmark_dir
-
+cd ${benchmark_dir}
 python train.py 
 
 deactivate
