49c4e8ba4dbf814082f6b2f2e3cb00335b6682a4
zzheng33 <zz695920754@gmail.com>
2024-09-24 02:39:20 +0000

    /

--- GPGPU/script/exp_power_motif.py
+++ GPGPU/script/exp_power_motif.py
@@ -51,7 +51,7 @@ altis_benchmarks_2 = ['cfd','cfd_double','fdtd2d','kmeans','lavamd',
                       'nw','particlefilter_float','particlefilter_naive','raytracing',
                       'srad','where']
 
-ecp_benchmarks = ['XSBench','miniGAN','CRADL','sw4lite','Laghos', 'UNet', 'Resnet50']
+ecp_benchmarks = ['XSBench','miniGAN','CRADL','sw4lite','Laghos', 'UNet', 'Resnet50','bert']
 ML = ['UNet', 'Resnet50']
 
 # Setup environment
