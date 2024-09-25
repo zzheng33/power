808fe2f780e1a757c43e99654ef0d5f680a0df44
zzheng33 <zz695920754@gmail.com>
2024-09-25 14:08:53 +0000

    /

--- GPGPU/script/start_power_motif.sh
+++ GPGPU/script/start_power_motif.sh
@@ -26,13 +26,13 @@
 # Laghos 200, 500, 5
 # XSBench 200, 500, 5
 
-# python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.4 --uncore_1 2.4 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --benchmark bert_large
+python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.4 --uncore_1 2.4 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --benchmark bert_large
 
-# sleep 60
+sleep 60
 
 
-# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/*.csv /home/cc/power/GPGPU/data/ecp_power_res/max_uncore 
-# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/max_uncore
+sudo mv /home/cc/power/GPGPU/data/ecp_power_res/*.csv /home/cc/power/GPGPU/data/ecp_power_res/max_uncore 
+sudo mv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/max_uncore
 
 
 # python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --benchmark bert_large
@@ -45,12 +45,12 @@
 
 
 
-python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --benchmark bert_large
+# python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --benchmark bert_large
 
-# sleep 60
+# # sleep 60
 
-sudo mv /home/cc/power/GPGPU/data/ecp_power_res/*.csv /home/cc/power/GPGPU/data/ecp_power_res/dynamic_uncore 
-sudo mv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/dynamic_uncore
+# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/*.csv /home/cc/power/GPGPU/data/ecp_power_res/dynamic_uncore 
+# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/mem_throughput/dynamic_uncore
 
 
 ./power_util/set_uncore_freq.sh 2.4 2.4
