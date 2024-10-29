dace614a061599062b5c705deab6223b617a0ed8
zzheng33 <zz695920754@gmail.com>
2024-10-29 16:25:43 +0000

    /

--- GPGPU/script/start_power_cap.sh
+++ GPGPU/script/start_power_cap.sh
@@ -7,7 +7,11 @@
 
 python3 exp_power_cap.py --suite 0 --benchmark_size 0
 python3 exp_power_cap.py --suite 0 --benchmark_size 0
-python3 exp_power_cap.py --suite 0 --benchmark_size 0
+
+
+python3 exp_power_cap.py --suite 1 --benchmark_size 0
+python3 exp_power_cap.py --suite 1 --benchmark_size 0
+
 
 
 # python3 exp_power_cap.py --benchmark maxflops --suite 1
