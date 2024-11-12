c94066a9bc7c06999e6d68aebde6da5246bdd414
zzheng33 <zz695920754@gmail.com>
2024-11-12 23:08:18 +0000

    /

--- GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/UNet-checkpoint.sh
+++ GPGPU/script/run_benchmark/ecp_script/.ipynb_checkpoints/UNet-checkpoint.sh
@@ -6,6 +6,7 @@ benchmark_dir="${home_dir}/benchmark/ECP/UNet/"
 cd ${benchmark_dir}
 source UNet_env/bin/activate
 # export OMP_NUM_THREADS=32
+wandb offline
 python train.py --amp -e 1
 
 # torchrun --nproc_per_node=4 train.py --amp -e 1
