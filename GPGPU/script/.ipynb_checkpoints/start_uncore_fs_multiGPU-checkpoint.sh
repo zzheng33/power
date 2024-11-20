1c41f7842a881f6f060aa6b86b34088564eab8bb
zzheng33 <zz695920754@gmail.com>
2024-11-20 19:43:42 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_uncore_fs_multiGPU-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_uncore_fs_multiGPU-checkpoint.sh
@@ -44,7 +44,7 @@
 
 
 
-python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark lammps
+python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark lammps
 
 sleep 5
 
