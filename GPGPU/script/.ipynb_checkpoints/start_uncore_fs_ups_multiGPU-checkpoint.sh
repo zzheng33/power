41551ebc1a0d5a8a3dbb7da30097986c3b9f1e0c
zzheng33 <zz695920754@gmail.com>
2024-11-20 16:03:49 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_uncore_fs_ups_multiGPU-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_uncore_fs_ups_multiGPU-checkpoint.sh
@@ -15,12 +15,12 @@
 ################### ECP Starts ###################
 
 
-python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 1 --benchmark miniGAN
+python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 1 --benchmark gromacs_multiGPU
 
-sleep 5
+# sleep 5
 
-sudo mv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/multiGPU/ups_uncore 
-sudo mv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/multiGPU/mem_throughput/ups_uncore
+# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/multiGPU/ups_uncore 
+# sudo mv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/ecp_power_res/no_power_shift/multiGPU/mem_throughput/ups_uncore
 
 
 ################### ECP Ends ###################
