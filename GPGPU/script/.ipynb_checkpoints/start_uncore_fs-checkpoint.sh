bbcec23db281be21bc60916663d4735ae2967e3c
zzheng33 <zz695920754@gmail.com>
2024-11-26 20:19:11 +0000

    /

--- GPGPU/script/.ipynb_checkpoints/start_uncore_fs-checkpoint.sh
+++ GPGPU/script/.ipynb_checkpoints/start_uncore_fs-checkpoint.sh
@@ -35,7 +35,7 @@ sudo mv /home/cc/power/GPGPU/data/npb_power_res/no_power_shift/mem_throughput/*.
 
 
 
-python3 exp_power_motif.py --suite 2 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 
+python3 exp_power_motif.py --suite 2 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 --memory_throughput_ts 40000
 
 sleep 5
 
--- GPGPU/script/start_uncore_fs_ups.sh
+++ GPGPU/script/start_uncore_fs_ups.sh
@@ -13,6 +13,17 @@
 #lammps 200,500,5
 
 
+################### NPB starts ###################
+
+python3 exp_power_motif.py --suite 2 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 1 
+
+sleep 5
+
+sudo mv /home/cc/power/GPGPU/data/npb_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/npb_power_res/no_power_shift/ups_uncore 
+sudo mv /home/cc/power/GPGPU/data/npb_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/npb_power_res/no_power_shift/mem_throughput/ups_uncore
+
+################### NPB ends ###################
+
 
 
 
@@ -40,7 +51,7 @@
 ################### ECP Starts ###################
 
 
-python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 1 
+# python3 exp_power_motif.py --suite 0 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 1 
 
 # sleep 5
 
