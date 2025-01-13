d1c6845325b0ea8bcce8e08b3e4ed4be6a3089bb
zzheng33 <zz695920754@gmail.com>
2025-01-13 16:44:57 +0000

    /

--- GPGPU/script/start_uncore_fs.sh
+++ GPGPU/script/start_uncore_fs.sh
@@ -42,13 +42,13 @@
 ################### ALTIS Starts ###################
 
 
-# python3 exp_power_motif.py --suite 1 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark srad
+python3 exp_power_motif.py --suite 1 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 2.2 --uncore_1 2.2 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark srad
 
-# sleep 5
+sleep 5
 
 
-# sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/max_uncore 
-# sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/max_uncore
+sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/max_uncore 
+sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/max_uncore
 
 
 # python3 exp_power_motif.py --suite 1 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 0 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 0 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark raytracing
@@ -60,12 +60,13 @@
 # sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/min_uncore
 
 
-python3 exp_power_motif.py --suite 1 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark gups
-sleep 5
+# python3 exp_power_motif.py --suite 1 --test 0  --dynamic_ufs_gpuP 0 --dynamic_ufs_mem 1 --uncore_0 0.8 --uncore_1 0.8 --pcm 1 --inc_ts 200 --dec_ts 500 --history 5 --dual_cap 1 --burst_up 0.4 --burst_low 0.2 --ups 0 --benchmark srad
+
+# sleep 5
 
 
-sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/dynamic_uncore 
-sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/dynamic_uncore
+# sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/dynamic_uncore 
+# sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/*.csv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/dynamic_uncore
 
 
 
@@ -113,7 +114,7 @@ sudo mv /home/cc/power/GPGPU/data/altis_power_res/no_power_shift/mem_throughput/
 
 
 
-./power_util/set_uncore_freq.sh 2.4 2.4
+./power_util/set_uncore_freq.sh 2.2 2.2
 
 
 
